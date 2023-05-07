12787592f16def70e0097b73549c255a590d43ef
saif80ali <mdsaif80ali@gmail.com>
2023-05-07 11:58:31 +0530

    Assignment 3 Handling Navigation with Routes

---
+++ assignments/src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/" >
+          My Page
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/" aria-current="page" >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
